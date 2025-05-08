b390ff9e4d9ce720db7f10fb4f258c5501df2001
makiam <maxtemplaar@gmail.com>
2025-05-08 23:46:00 +0300

    Flask test App

--- App.py
+++ App.py
@@ -3,8 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template("Index.html")
+def hello()-> str:
+    context: dict[str, str] = {'name': 'Vetrarljos'}
+    return render_template("Index.html", **context)
 
 if __name__ == "__main__":
     app.run()
